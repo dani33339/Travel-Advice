c3ea277dd88c92034a3c36a253eb5cae854ce3f8
kiliman1995@gmail.com <kiliman1995@gmail.com>
2021-12-05 11:54:45 +0200

    fill the database with some trips

--- trips/views.py
+++ trips/views.py
@@ -2,24 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Trip
 
-TripsSList = [
 
-{'id': '1', 
-'title': 'Morocco',
- 'description': "The one that explores Morocco's famous souks, mosques and scenery over 5 days" 
- },
-
-{ 'id': '2',
- 'title': 'European Discovery',
-  'description': "The one that uncovers Europe’s finest in just under 2 weeks, complete with Bavarian beer and gondola rides" 
-  },
-
-{'id': '3',
- 'title': 'Vietnam Experience',
-  'description': 'The one that brings you the best of Vietnam, from colourful cities to epic natural wonders' 
-  }
-
-]
 
 def trips(request): 
     trips = Trip.objects.all()
