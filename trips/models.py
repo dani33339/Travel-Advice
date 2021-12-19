897cb86f3e407818b46c93cf94c2c3c470fc1139
Daniel Markov <dani33339@gmail.com>
2021-12-19 22:07:07 +0200

    updated trip unit tests
    updated trip unit tests

--- trips/models.py
+++ trips/models.py
@@ -43,7 +43,7 @@ class Review(models.Model):
         ('up', 'Up Vote'),
         ('down', 'Down Vote'),
     )
-    owner =models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
     trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
     body = models.TextField(null=True, blank=True)
     value = models.CharField(max_length=200, choices= VOTE_TYPE)
